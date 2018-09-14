53c74a55428e6d9d117aeff999eaab0222cf2ae4
Summer <roxy090128@gmail.com>
2018-09-14 16:51:58 -0500

    Add files via upload

---
+++ client.lua
@@ -0,0 +1,20 @@
+--Give Weapons Unlimited Weapon/Ammo
+
+Citizen.CreateThread(function()
+    while true do
+    Citizen.Wait(0) -- 3000    
+	local ped = GetPlayerPed(-1)
+	
+	local ball = GetHashKey("WEAPON_BALL")
+	GiveWeaponToPed(ped, ball, 1)
+	SetPedInfiniteAmmo(ped, true, ball)
+	
+	local snow = GetHashKey("WEAPON_SNOWBALL")
+	GiveWeaponToPed(ped, snow, 1)
+	SetPedInfiniteAmmo(ped, true, snow)
+	
+	
+end
+end)
+
+
